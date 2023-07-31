d4a65ab45429c0480bcf50019c76e4e0f2b15c9b
schumijo <j.joussot@free.fr>
2023-07-31 10:59:10 +0200

    Add equidia

---
+++ parser/equidia.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3
+# Thanks to pshanmu3 user on github
+import requests
+import os
+import sys
+
+proxies = {}
+if len(sys.argv) == 3:
+    proxies = {
+                'http' : sys.argv[2],
+                'https' : sys.argv[2]
+              }
+
+na = 'https://raw.githubusercontent.com/naveenland4/UTLive/main/assets/info.m3u8'
+def grab(id):
+    headers={
+        "User-Agent": "Equidia/6036 CFNetwork/1220.1 Darwin/20.3.0",
+        "Referer": "https://fr.equidia.app/"
+    }
+    try:
+        m3u = s.get('https://api.equidia.fr/api/public/racing/equidia-mobileapp-ios-1/equidia-'+id, headers=headers, proxies=proxies).json()['primary']
+    except Exception as e:
+        m3u = na
+    finally:
+        print(m3u)
+
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+s = requests.Session()
+
+if (sys.argv[1] == 'equidia'):
+    id = 'live2'
+elif (sys.argv[1] == 'trot'):
+    id = 'racingtrot'
+elif (sys.argv[1] == 'galop'):
+    id = 'racinggalop'
+elif (sys.argv[1] == 'mag'):
+    id = 'racingmag'
+else:
+    sys.exit(1)
+
+grab(id)
---
+++ parser/equidia.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 parser/equidia.py equidia http://62.210.135.99:80 > playlists/equidia/equidia.m3u8
+
+exit 0

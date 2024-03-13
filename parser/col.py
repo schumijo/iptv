5222f54c5c8b5c2fb932e8dcadb8f7653232368b
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-13 14:11:01 +0100

    Update col.py

--- parser/col.py
+++ parser/col.py
@@ -4,14 +4,14 @@ import os
 import sys
 
 na = 'https://github.com/BG47510/Zap/raw/main/assets/error.m3u8'
-def grab():
+def grab(url):
     headers={
         "Referer": "http://callofliberty.fr/",
         "Origin": "http://callofliberty.fr",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
     }
     try:
-        m3u = s.get('http://s2.callofliberty.fr/direct/TF1/master.m3u8', headers=headers)
+        m3u = s.get(url, headers=headers)
     except Exception as e:
         m3u = na
     finally:

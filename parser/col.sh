c88dcbb7405e6c039d492b581da93afe692362a4
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-13 14:04:35 +0100

    Create col.sh

---
+++ parser/col.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 parser/col.py http://s2.callofliberty.fr/direct/TF1/master.m3u8 > playlists/col/tf1.m3u8
+
+exit 0
--- parser/tf1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 parser/tf1.py > playlists/tf1/tf1.m3u8
-
-exit 0

febecfe34f1a854594088c6b1cd894c5e2550c24
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-13 14:13:19 +0100

    Update col.sh

--- parser/col.sh
+++ parser/col.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 python3 parser/col.py http://s2.callofliberty.fr/direct/TF1/master.m3u8 > playlists/col/tf1.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct/W9/master.m3u8  > playlists/col/w9.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct/TFX/master.m3u8 > playlists/col/tfx.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct/TF1SERIESFILMS/master.m3u8 > playlists/col/tf1sf.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct/6ter/master.m3u8 > playlists/col/6ter.m3u8
 
 exit 0

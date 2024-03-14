71df8b114f020e46afd248ffaae9271da64eef5e
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-14 17:12:56 +0100

    Update jdg-tv.py

--- parser/jdg-tv.py
+++ parser/jdg-tv.py
@@ -3,10 +3,17 @@ import requests
 import os
 import sys
 
+proxies = {}
+if len(sys.argv) == 2:
+    proxies = {
+                'http' : sys.argv[1],
+                'https' : sys.argv[1]
+              }
+
 na = 'https://raw.githubusercontent.com/naveenland4/UTLive/main/assets/info.m3u8'
 def grab():
     try:
-        m3u = s.get('https://www.dailymotion.com/player/metadata/video/k5VKYQn5hAE4vfry927?embedder=https%3A%2F%2Fwww.journaldugolf.fr').json()['qualities']['auto'][0]['url']
+        m3u = s.get('https://www.dailymotion.com/player/metadata/video/k5VKYQn5hAE4vfry927?embedder=https%3A%2F%2Fwww.journaldugolf.fr', proxies=proxies).json()['qualities']['auto'][0]['url']
     except Exception as e:
         m3u = na
         print(na)

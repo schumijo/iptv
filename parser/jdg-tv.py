a568041aae84a7a4a98b5071a5f4c7942d2d28fb
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-14 17:20:02 +0100

    Update jdg-tv.py

--- parser/jdg-tv.py
+++ parser/jdg-tv.py
@@ -17,7 +17,7 @@ def grab():
         m3u = s.get('https://www.dailymotion.com/player/metadata/video/k5VKYQn5hAE4vfry927?embedder=https://www.journaldugolf.fr', proxies=proxies).json()['qualities']['auto'][0]['url']
     except Exception as e:
         m3u = na
-        print(na)
+        print(e)
         sys.exit(1)
 
     try:

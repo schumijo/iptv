81a85b927edc09a1a543e104582f7dad074c51ba
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-15 14:46:27 +0100

    Update lequipe.sh

--- parser/lequipe.sh
+++ parser/lequipe.sh
@@ -3,5 +3,6 @@
 python3 parser/lequipe.py k1kypsRZF9plQhqwBRS http://51.83.116.6:22754 > playlists/lequipe/lequipelive1.m3u8
 python3 parser/lequipe.py k5TgcOKBUTM2KnqwBWC http://51.83.116.6:22754 > playlists/lequipe/lequipelive2.m3u8
 python3 parser/lequipe.py kXRfcKHV9HhcZuqwBYP http://51.83.116.6:22754 > playlists/lequipe/lequipelive3.m3u8
+python3 parser/lequipe.py k6oih7JyuEmhrnqwBZT http://51.83.116.6:22754 > playlists/lequipe/lequipelive4.m3u8
 
 exit 0

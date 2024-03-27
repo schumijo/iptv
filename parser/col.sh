0b4d05eaeafb91c0c7791356c307ba205fbdc89e
schumijo <40740060+schumijo@users.noreply.github.com>
2024-03-27 13:54:53 +0100

    Update col.sh

--- parser/col.sh
+++ parser/col.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 parser/col.py http://s2.callofliberty.fr/direct/TF1/master.m3u8 > playlists/col/tf1.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/W9/index.m3u8  > playlists/col/w9.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/TFX/master.m3u8 > playlists/col/tfx.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/TF1SERIESFILMS/master.m3u8 > playlists/col/tf1sf.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/6ter/index.m3u8 > playlists/col/6ter.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/NRJ12/index.m3u8 > playlists/col/nrj12.m3u8
-python3 parser/col.py http://s2.callofliberty.fr/direct/Cherie25/index.m3u8 > playlists/col/cherie25.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/TF1/master.m3u8 > playlists/col/tf1.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/W9/index.m3u8  > playlists/col/w9.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/TFX/master.m3u8 > playlists/col/tfx.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/TF1SERIESFILMS/master.m3u8 > playlists/col/tf1sf.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/6ter/index.m3u8 > playlists/col/6ter.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/NRJ12/index.m3u8 > playlists/col/nrj12.m3u8
+python3 parser/col.py http://s2.callofliberty.fr/direct1/Cherie25/index.m3u8 > playlists/col/cherie25.m3u8
 
 exit 0

84fc0fbde4db7660e81aefc192c1213efbeb2151
schumijo <40740060+schumijo@users.noreply.github.com>
2024-04-20 14:35:55 +0200

    Update cstar.sh

--- parser/cstar.sh
+++ parser/cstar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 python3 parser/cstar.py x5gv5v0 http://51.83.116.6:22754 > playlists/cstar/cstar.m3u8
+python3 parser/cstar.py x5gv5rr http://51.83.116.6:22754 > playlists/canalplus/c8.m3u8
+
 
 exit 0

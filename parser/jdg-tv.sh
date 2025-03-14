b8bbbb721cd6f913f7042e0db88d43c03bbd0487
schumijo <40740060+schumijo@users.noreply.github.com>
2025-03-14 15:18:18 +0100

    Delete parser directory

--- parser/jdg-tv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 parser/jdg-tv.py http://51.83.116.6:22754 > playlists/jdg-tv/jdg-tv.m3u8
-
-exit 0
--- parser/w9.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 parser/w9.py > playlists/w9/w9.m3u8
-
-exit 0

0402cfaf8f62d79d06c0c7b9661dafd9add649ff
schumijo <40740060+schumijo@users.noreply.github.com>
2025-03-14 11:51:24 +0100

    Update samsung + remove sony

--- tools/merge.sh
+++ tools/merge.sh
@@ -2,7 +2,6 @@
 
 cat playlists/iptv/fr.m3u8 > fr.m3u8
 tail -n +2 playlists/iptv/fr_mytf1_fast.m3u8 >> fr.m3u8
-tail -n +2 playlists/iptv/fr_sony.m3u8 >> fr.m3u8
-wget https://i.mjh.nz/SamsungTVPlus/fr.m3u8 -O - >> fr.m3u8
+tail -n +2 playlists/iptv/fr_samsungtv.m3u8 >> fr.m3u8
 
 exit 0

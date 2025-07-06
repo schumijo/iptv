44db9b9526789f6c4fb607c6a0dd70b3ff2cb7ac
schumijo <40740060+schumijo@users.noreply.github.com>
2025-07-06 10:18:05 +0200

    Fix encoding

--- tools/merge.sh
+++ tools/merge.sh
@@ -2,6 +2,7 @@
 
 cat playlists/iptv/fr.m3u8 > fr.m3u8
 tail -n +2 playlists/iptv/fr_mytf1_fast.m3u8 >> fr.m3u8
-tail -n +2 playlists/iptv/fr_samsungtv.m3u8 >> fr.m3u8
+iconv -f ISO-8859-1 -t UTF-8 playlists/iptv/fr_samsungtv.m3u8 > playlists/iptv/fr_samsungtv_corrected.m3u8
+tail -n +2 playlists/iptv/fr_samsungtv_corrected.m3u8 >> fr.m3u8
 
 exit 0

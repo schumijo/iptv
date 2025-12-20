e582e38d0b3aa28a2a06be9c067bd2ef12552aba
schumijo <40740060+schumijo@users.noreply.github.com>
2025-12-20 15:46:02 +0100

    Change tail command to append last line only

--- tools/merge.sh
+++ tools/merge.sh
@@ -2,6 +2,6 @@
 
 cat playlists/iptv/fr.m3u8 > fr.m3u8
 #tail -n +2 playlists/iptv/fr_mytf1_fast.m3u8 >> fr.m3u8
-tail -n +2 playlists/iptv/fr_samsungtv.m3u8 >> fr.m3u8
+tail -n +1 playlists/iptv/fr_samsungtv.m3u8 >> fr.m3u8
 
 exit 0
